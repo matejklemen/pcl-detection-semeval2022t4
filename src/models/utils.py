86343aca10be51c451cc522c97ba19f0fb33393f
Matej Klemen <matej.klemen1337@gmail.com>
2022-01-01 14:45:06 +0100

    Modify possible models

--- src/models/utils.py
+++ src/models/utils.py
@@ -3,7 +3,7 @@ from transformers import BertTokenizerFast, DistilBertTokenizerFast, RobertaToke
 
 def load_fast_tokenizer(tokenizer_type, pretrained_name_or_path):
     # There is no AutoTokenizerFast??
-    assert tokenizer_type in ["bert", "distilbert", "roberta", "xlm-roberta", "t5", "mt5", "electra", "xlnet"]
+    assert tokenizer_type in ["bert", "distilbert", "roberta", "xlm-roberta", "xlnet"]
 
     if tokenizer_type == "bert":
         return BertTokenizerFast.from_pretrained(pretrained_name_or_path)
@@ -13,11 +13,5 @@ def load_fast_tokenizer(tokenizer_type, pretrained_name_or_path):
         return RobertaTokenizerFast.from_pretrained(pretrained_name_or_path)
     elif tokenizer_type == "xlm-roberta":
         return XLMRobertaTokenizerFast.from_pretrained(pretrained_name_or_path)
-    elif tokenizer_type == "t5":
-        return T5TokenizerFast.from_pretrained(pretrained_name_or_path)
-    elif tokenizer_type == "mt5":
-        return MT5TokenizerFast.from_pretrained(pretrained_name_or_path)
-    elif tokenizer_type == "electra":
-        return ElectraTokenizerFast.from_pretrained(pretrained_name_or_path)
     elif tokenizer_type == "xlnet":
         return XLNetTokenizerFast.from_pretrained(pretrained_name_or_path)
